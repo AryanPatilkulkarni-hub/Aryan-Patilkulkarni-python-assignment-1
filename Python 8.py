a98e9730f0dd3758b3818214027599a7d21e53fb
AryanPatilkulkarni-hub <aryanpatilkulkarni@gmail.com>
2025-11-22 22:17:49 +0530

    Add functionality to cube odd numbers from input list
    Output a dictionary of odd numbers and their cubes from the input list using list comprehension.

---
+++ Python 8.py
@@ -0,0 +1,9 @@
+"""
+8.	Output dictionary which contains only the odd numbers that are present in the input list as keys and their cubes as values using List Comprehension
+"""
+
+x = int(input("Please enter how many numbers you want to enter into the list: "))
+n = [int(input("Enter the number: ")) for i in range(x)]
+oddvalue = {num: num**3 for num in n if num % 2 != 0}
+for x,y in oddvalue.items():
+    print(x, ":", y)
